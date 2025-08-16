8c65181df864f9960dfc86bbfd462824ab0f3e3d
kushasahu7 <kusha.mil@gmail.com>
2025-08-16 15:04:20 +0530

    i will try this

--- microfrontends/blog/src/App.tsx
+++ microfrontends/blog/src/App.tsx
@@ -7,11 +7,11 @@ function App() {
   return (
     <Router>
       <div className="min-h-screen bg-white">
-        <Routes>
-          <Route path="/" element={<BlogList />} />
-          <Route path="/roadmap" element={<Roadmap />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+              <Routes>
+        <Route path="/blog" element={<BlogList />} />
+        <Route path="/blog/roadmap" element={<Roadmap />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
       </div>
     </Router>
   );
